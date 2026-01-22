7b7ca86fc955c4b219dc9fd6147ca992849e32e9
edugrowth5-rgb <edugrowth5@gmail.com>
2026-01-22 22:43:41 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,37 @@
-const CACHE_NAME = 'clock-v1';
-const assets = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'hyper-clock-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://www.soundjay.com/nature/rain-01.mp3'
+];
 
+// Install Service Worker
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(assets)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching assets for offline use');
+      return cache.addAll(assets);
+    })
+  );
 });
 
+// Activate & Cleanup old caches
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }));
+    })
+  );
+});
+
+// Fetching assets from cache
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
 });
