91c1b1922c7827290b1bda13b38c0add81979dfe
edugrowth5-rgb <edugrowth5@gmail.com>
2026-01-22 23:03:22 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'hyper-clock-v5';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://www.soundjay.com/nature/rain-01.mp3'
+];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
